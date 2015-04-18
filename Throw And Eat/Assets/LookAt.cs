a5fcf7c5b151a2029da9773d0877b33d54ad5967
3drick <the3drick@gmail.com>
2015-04-18 20:55:41 +0200

    Add eyes

---
+++ Throw And Eat/Assets/LookAt.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+
+	public GameObject target;
+
+	void Update () {
+		transform.LookAt(target.transform);
+	}
+}
