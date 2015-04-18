a355dd2d5f9e35f8b9c220b8937c3daa92767389
Niels Horn <Niels@Programmer.net>
2015-04-18 10:14:06 +0200

    Add player movement

--- Throw And Eat/Assets/Scripts/Player/Controller.cs
+++ Throw And Eat/Assets/Scripts/Player/Controller.cs
@@ -3,13 +3,54 @@ using System.Collections;
 
 public class Controller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private float forwardSpeed;
+	private float sidewaysSpeed;
+
+	private float sidewaysRot;
+	private float verticalRot;
+
+	private float verticalVel = 0;
+
+	private Vector3 speed;
+
+	private CharacterController characterController;
+
+	public float movSpeed = 8.0F;
+	public float mouseSensitivity = 2.0F;
+	public float verticalRotRange = 60.0F;
+	public float jumpHeight = 5.0F;
+
+	void Awake() {
+
+		characterController = GetComponent<CharacterController>();
 	}
+
+	void Update() {
 	
-	// Update is called once per frame
-	void Update () {
-	
+		sidewaysRot = Input.GetAxis("Mouse X") * mouseSensitivity;
+		verticalRot -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+				
+		transform.Rotate(0, sidewaysRot, 0);
+				
+		verticalRot = Mathf.Clamp(verticalRot, -verticalRotRange, verticalRotRange);
+				
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRot, 0, 0);
+				
+				
+		forwardSpeed = Input.GetAxis("Vertical") * movSpeed;
+		sidewaysSpeed = Input.GetAxis("Horizontal") * movSpeed;
+				
+		if (Input.GetButton("Jump") && characterController.isGrounded) {
+
+			verticalVel = jumpHeight;
+		}
+					
+		verticalVel += Physics.gravity.y * Time.deltaTime;		
+						
+		speed = new Vector3(sidewaysSpeed, verticalVel, forwardSpeed);
+						
+		speed = transform.rotation * speed;
+						
+		characterController.Move(speed * Time.deltaTime);
 	}
 }
