c2123bbe7bdf11c4e837fb51dd2b67096156753a
Asger Horn Borholt <asgerhb@gmail.com>
2018-09-14 19:37:57 +0200

    Remove breadcrumbs particle game object when animation finishes

---
+++ Throw And Eat/Assets/Scripts/AutoDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestruct : MonoBehaviour {
+
+	public float delay;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, delay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
