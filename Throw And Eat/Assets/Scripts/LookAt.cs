ceda9e13a0f2dc7ea58628108e71adbccba184e1
Asger Horn Borholt <asgerhb@gmail.com>
2018-09-14 20:37:55 +0200

    Make enemies stare at player camera and not body

--- Throw And Eat/Assets/Scripts/LookAt.cs
+++ Throw And Eat/Assets/Scripts/LookAt.cs
@@ -6,8 +6,8 @@ public class LookAt : MonoBehaviour {
 	public GameObject target;
 
 	void Awake() {
-
-		target = GameObject.FindGameObjectWithTag("Player");
+		if(target == null)
+			target = GameObject.FindGameObjectWithTag("MainCamera");
 	}
 
 	void Update() {
